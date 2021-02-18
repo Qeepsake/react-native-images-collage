90eac4696c068fc512144cc52c50fad30d9d4e4c
Luke Brandon Farrell <lukebrandonfarrell@gmail.com>
2021-02-18 11:14:49 +0000

    Update src/DynamicCollage.js

--- src/DynamicCollage.js
+++ src/DynamicCollage.js
@@ -10,7 +10,7 @@ class DynamicCollage extends React.Component {
     super(props);
 
     this.state = {
-      imageFocusId: "",
+      imageFocusId: null,
       images: props.images,
       collageWidth: null,
       collageHeight: null,
